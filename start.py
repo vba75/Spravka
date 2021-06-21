ea858c2caa0a135b7bcbb99973006a70edaf71c3
Vladimir Bychkov <vreboot@yandex.ru>
2021-06-21 20:14:19 +0900

    Добавлено описание к программе в файле README

--- start.py
+++ start.py
@@ -3,48 +3,46 @@ import requests
 
 #https://www.spravka333333.ru/currency?date=2021-06-08
 
+def getMoney():
+	#URL = 'https://www.spravka333333.ru/currency-data/full.json'
+	URL= 'https://www.spravka333333.ru/currency'
+	html= requests.get(URL)
+
+	#print(html.text)
+
+
+	model = BeautifulSoup(html.text, 'html.parser')
+	data = model.find(class_='onDate c-calendar').get_text()
+	last_data = data.strip()
 
 
 
-#URL = 'https://www.spravka333333.ru/currency-data/full.json'
-URL= 'https://www.spravka333333.ru/currency'
-html= requests.get(URL)
+	table_main = model.find(class_ ='currency sorter tablesorter {sortlist: [[1,1]]}').tbody
+	trs = table_main.find_all('tr')
 
-#print(html.text)
+	print(data)
+	print('-----------------------------------')
 
+	for tr in trs:
 
-model = BeautifulSoup(html.text, 'html.parser')
-data = model.find(class_='onDate c-calendar').get_text()
-last_data = data.strip()
+		bank_name = tr.td.div.div.get_text()
+		bank_address =  tr.span.get_text()
+		bank_phone   =  tr.find(class_='fa fa-phone')['href']
 
+		tds = tr.find_all('td')
 
+		dollar_bay  = tds[1].get_text()
+		dollar_sail = tds[2].get_text()
 
-table_main = model.find(class_ ='currency sorter tablesorter {sortlist: [[1,1]]}').tbody
-trs = table_main.find_all('tr')
+		euro_bay  = tds[3].get_text()
+		euro_sail = tds[4].get_text()
 
-print(data)
-print('-----------------------------------')
+		yan_bay   = tds[5].get_text()
+		yan_sail  = tds[6].get_text()
 
-for tr in trs:
-
-	bank_name = tr.td.div.div.get_text()
-	bank_address =  tr.span.get_text()
-	bank_phone  = tr.find(class_='fa fa-phone')['href']
-
-	tds = tr.find_all('td')
-
-	dollar_bay  = tds[1].get_text()
-	dollar_sail = tds[2].get_text()
-
-	euro_bay  = tds[3].get_text()
-	euro_sail = tds[4].get_text()
-
-	yan_bay   = tds[5].get_text()
-	yan_sail  = tds[6].get_text()
-
-	yena_bay  = tds[7].get_text()
-	yena_sail = tds[8].get_text()
-	print( bank_name , bank_address , bank_phone, '>>> ' , dollar_bay , dollar_sail, euro_bay,  euro_sail , yan_bay, yan_sail, yena_bay, yena_sail )
+		yena_bay  = tds[7].get_text()
+		yena_sail = tds[8].get_text()
+		print( bank_name , bank_address , bank_phone, '>>> ' , dollar_bay , dollar_sail, euro_bay,  euro_sail , yan_bay, yan_sail, yena_bay, yena_sail )
 
 
 
